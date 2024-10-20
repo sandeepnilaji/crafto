c57b8df378966408abea8d48cb53f50e87f04513
Sandeep <sandeep@Sandeeps-MacBook-Air.local>
2024-10-20 14:03:55 +0530

    Fixed create quote textarea input issue

--- app/quotes/create/page.jsx
+++ app/quotes/create/page.jsx
@@ -79,7 +79,7 @@ export default function CreateQuotePage() {
             Create Quote
           </h1>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="h-[100px]">
+            <div className="mb-4">
               <label
                 htmlFor="text"
                 className="block text-sm font-medium text-gray-700"
@@ -89,13 +89,13 @@ export default function CreateQuotePage() {
               <textarea
                 id="text"
                 {...register("text")}
-                className="text-neutral-950 mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-600 focus:border-gray-500"
+                className="text-neutral-950 mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-600 focus:border-gray-500 resize-y min-h-[100px]"
               />
               {errors.text && (
                 <p className="mt-1 text-sm text-red-600">{errors.text.message}</p>
               )}
             </div>
-            <div className="h-[100px]">
+            <div className="mb-4">
               <label
                 htmlFor="image"
                 className="block text-sm font-medium text-gray-700"
